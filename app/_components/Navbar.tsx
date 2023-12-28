852f0bb6019fbd14b4e3b4da9d02955a7b5fa8fc
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-12-28 11:59:39 +0100

    start navbar

---
+++ app/_components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+const navLinks = [
+  { label: "About Us", href: "#" },
+  { label: "Services", href: "#" },
+  { label: "Pricing", href: "#" },
+  { label: "Book Now", href: "#" },
+];
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between h-[84px] items-center px-16">
+      <div className="">
+        <Link href="/">Logo Here</Link>
+      </div>
+      <ul className="flex gap-16">
+        {navLinks.map((navLink) => (
+          <li key={navLink.label}>
+            <Link href={navLink.href}>{navLink.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
