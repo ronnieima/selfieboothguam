d137159e807842ec34eb78ab17a51a82fac74d2a
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-12-30 15:13:38 +0100

    change default theme to light

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={fontLibreBakersville.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
