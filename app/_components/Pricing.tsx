eb74b4c9b76fb715941d58fd62080f8328b4e78b
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-01-07 00:03:19 +0100

    add translations

--- app/_components/Pricing.tsx
+++ app/_components/Pricing.tsx
@@ -1,13 +1,17 @@
 import { cn } from "@/lib/utils";
 import PriceCards from "./ui/PriceCards";
+import { useTranslations } from "next-intl";
 
 function Pricing() {
+  const t = useTranslations("Pricing");
   return (
     <section
       id="pricing"
       className="bg-background flex flex-col items-center gap-8 py-16 lg:max-w-[80rem] mx-auto scroll-m-16"
     >
       <header className="flex flex-col items-center">
+        <h2 className={` text-center text-4xl md:text-6xl `}>{t("header")}</h2>
+        <span>{t("subtitle")}</span>
         <h2 className={` text-center text-4xl md:text-6xl `}>Pricing</h2>
         <span className="text-muted-foreground">Billed hourly</span>
       </header>
