55a710b9d37bc110b3a39860b5696ba2e288aa67
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-01-08 07:56:46 +0100

    removed CDN links

--- messages/content.tsx
+++ messages/content.tsx
@@ -1,18 +1,16 @@
-import { yearsSince } from "@/lib/utils";
-
-const CLOUDINARY_URL =
-  "https://res.cloudinary.com/dfpbpun9z/image/upload/f_auto,q_auto/v1/selfieboothguam/";
+// const CLOUDINARY_URL =
+//   "https://res.cloudinary.com/dfpbpun9z/image/upload/f_auto,q_auto/v1/selfieboothguam/";
 
 export const socialLinks = {
   facebook: "https://www.facebook.com/profile.php?id=100063500533195",
 };
 
-export const imageLinks = {
-  props: CLOUDINARY_URL + "props",
-  photobooth: CLOUDINARY_URL + "photobooth1",
-  layout: CLOUDINARY_URL + "layout",
-  hero: CLOUDINARY_URL + "hero",
-};
+// export const imageLinks = {
+//   props: CLOUDINARY_URL + "props",
+//   photobooth: CLOUDINARY_URL + "photobooth1",
+//   layout: CLOUDINARY_URL + "layout",
+//   hero: CLOUDINARY_URL + "hero",
+// };
 
 export const prices = [
   { timeInHours: 2, priceInUSD: 250.0 },
