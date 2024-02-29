82d3c76ae3d9ca79a99171f553d2f8ab9181536b
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-02-29 09:53:31 +0100

    work on footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,11 +1,10 @@
 import { cn } from "@/lib/utils";
-import NavLinks from "./ui/NavLinks";
 import Link from "next/link";
-import LocaleSwitcherSelect from "./ui/LocaleSwitcherSelect";
+import NavLinks from "./ui/NavLinks";
 
 import img from "@/public/photobooth1.png";
+import { Mail, MapPin, Phone } from "lucide-react";
 import Image from "next/image";
-import { Mail, MapPin, Phone, Pin } from "lucide-react";
 
 function Footer() {
   return (
@@ -15,12 +14,12 @@ function Footer() {
       <div
         className={cn(
           "grid px-8 py-12 text-white",
-          `md:grid-cols-2`,
-          `lg:px-16 lg:py-8  lg:grid lg:grid-cols-4 gap-8`
+          `sm:grid-cols-2`,
+          `lg:px-16 lg:py-8 lg:grid lg:grid-cols-4 gap-8`
         )}
       >
         <div className='flex flex-col gap-4 '>
-          <div className='relative w-52  h-32'>
+          <div className='relative w-52 h-32'>
             <Image
               src={img}
               fill
@@ -37,14 +36,14 @@ function Footer() {
         </div>
 
         <div>
-          <h2 className='text-2xl pb-2 text-semibold'>Browse</h2>
+          <h2 className='text-2xl pb-2 text-bold mb-4'>Browse</h2>
           <ul className='flex flex-col items-start gap-2'>
             <NavLinks className='text-slate-100 text-left font-thin text-sm' />
           </ul>
         </div>
 
         <div>
-          <h2 className='text-2xl pb-2 text-semibold'>Services</h2>
+          <h2 className='text-2xl pb-2 text-bold mb-4'>Services</h2>
           <ul className='flex flex-col items-start gap-2'>
             <li>
               <Link href={`#`} className='hover:underline font-thin text-sm'>
@@ -65,22 +64,22 @@ function Footer() {
         </div>
 
         <div>
-          <h2 className='text-2xl pb-2 text-semibold'>Contact</h2>
+          <h2 className='text-2xl pb-2 text-bold mb-4'>Contact</h2>
           <ul className='flex flex-col items-start gap-2'>
             <li className='flex items-center gap-2 font-thin'>
-              <MapPin /> Dededo, Guam
+              <MapPin size={16} /> Dededo, Guam
             </li>
             <li className='flex items-center gap-2 font-thin'>
-              <Mail /> email@email.com
+              <Mail size={16} /> email@email.com
             </li>
             <li className='flex items-center gap-2 font-thin'>
-              <Phone /> 671-123-4567
+              <Phone size={16} /> 671-123-4567
             </li>
           </ul>
         </div>
       </div>
 
-      <div className='flex justify-between bg-slate-900 px-16 py-2 items-center'>
+      <div className='flex justify-between bg-slate-900 sm:px-16 px-4 py-2 items-center'>
         <span className='text-xs text-center text-muted-foreground'>
           Developed by{" "}
           <Link
